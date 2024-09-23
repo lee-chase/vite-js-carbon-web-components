712a68cab0b66034276b72e1d0c520437c519f8b
Lee Chase <lee.chase@uk.ibm.com>
2024-09-23 19:56:45 +0100

    feat: complete step 10

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 import { LitElement, html } from 'lit';
 import './style.scss';
 import '@carbon/web-components/es/components/button/button.js';
+import '@carbon/web-components/es/components/ui-shell/index';
+
+import Notification20 from '@carbon/web-components/es/icons/notification/20.js';
+import UserAvatar20 from '@carbon/web-components/es/icons/user--avatar/20.js';
+import Switcher20 from '@carbon/web-components/es/icons/switcher/20.js';
+
+export class MyIcon extends LitElement {
+  static properties = {
+    icon: { attribute: true, type: String },
+  };
+
+  render() {
+    return (
+      { Notification20, UserAvatar20, Switcher20 }[this.icon] ?? Notification20
+    )();
+  }
+}
+customElements.define('my-icon', MyIcon);
 
 export class MyCount extends LitElement {
   static properties = {
