85aa811515a5765545dd29d6a544ecf89c88af42
Lee Chase <lee.chase@uk.ibm.com>
2024-09-25 10:55:36 +0100

    chore: tidy main.js

--- main.js
+++ main.js
@@ -2,12 +2,3 @@ import './style.scss';
 import '@carbon/web-components/es/components/button/button.js';
 import '@carbon/web-components/es/components/ui-shell/index';
 import './components';
-
-import { LitElement, html } from 'lit';
-
-export class TutorialApp extends LitElement {
-  render() {
-    return html``;
-  }
-}
-customElements.define('tutorial-app', TutorialApp);
