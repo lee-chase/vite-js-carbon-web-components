d146da59e25363d1ae7aee3974e9302e48662adb
Lee Chase <lee.chase@uk.ibm.com>
2024-10-09 10:47:30 +0100

    fix: tidy page-repositories

--- components/page-repositories.js
+++ components/page-repositories.js
@@ -36,50 +36,50 @@ const data = [
 
 export class PageRepositories extends LitElement {
   render() {
-    return html`<div class="page--repositories">
-      <div class="${gridClasses({ fullWidth: true })}">
-        <div
-          class="${colClasses({
-            class: 'page--repositories__table',
-            sizes: { sm: 4, md: 8, lg: 16 },
-          })}"
-        >
-          <cds-table expandable>
-            <cds-table-header-title slot="title"
-              >Carbon Repositories</cds-table-header-title
-            >
-            <cds-table-header-description slot="description"
-              >A collection of public Carbon
-              repositories.</cds-table-header-description
-            >
-            <cds-table-head>
-              <cds-table-header-row>
-                ${headings.map(
-                  (heading) =>
-                    html`<cds-table-header-cell
-                      >${heading}</cds-table-header-cell
-                    >`,
-                )}
-              </cds-table-header-row>
-            </cds-table-head>
-            <cds-table-body>
-              ${data.map(
-                (row) => html`<cds-table-row>
-                    ${Object.keys(row).map((key) => {
-                      const cell = row[key];
-                      if (key !== 'expansion') {
-                        return html`<cds-table-cell data-key="${key}"
-                          >${cell}</cds-table-cell
-                        >`;
-                      }
-                    })} </cds-table-row
-                  ><cds-table-expanded-row
-                    >${row.expansion}</cds-table-expanded-row
+    return html`<div
+      class="${gridClasses({ class: 'page--repositories', fullWidth: true })}"
+    >
+      <div
+        class="${colClasses({
+          class: 'page--repositories__table',
+          sizes: { sm: 4, md: 8, lg: 16 },
+        })}"
+      >
+        <cds-table expandable>
+          <cds-table-header-title slot="title"
+            >Carbon Repositories</cds-table-header-title
+          >
+          <cds-table-header-description slot="description"
+            >A collection of public Carbon
+            repositories.</cds-table-header-description
+          >
+          <cds-table-head>
+            <cds-table-header-row>
+              ${headings.map(
+                (heading) =>
+                  html`<cds-table-header-cell
+                    >${heading}</cds-table-header-cell
                   >`,
               )}
-            </cds-table-body>
-          </cds-table>
-        </div>
+            </cds-table-header-row>
+          </cds-table-head>
+          <cds-table-body>
+            ${data.map(
+              (row) => html`<cds-table-row>
+                  ${Object.keys(row).map((key) => {
+                    const cell = row[key];
+                    if (key !== 'expansion') {
+                      return html`<cds-table-cell data-key="${key}"
+                        >${cell}</cds-table-cell
+                      >`;
+                    }
+                  })} </cds-table-row
+                ><cds-table-expanded-row
+                  >${row.expansion}</cds-table-expanded-row
+                >`,
+            )}
+          </cds-table-body>
+        </cds-table>
       </div>
     </div>`;
   }
