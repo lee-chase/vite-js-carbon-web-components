72d0d306acb5a75bab94fe9941db35fc0b44d643
Lee Chase <lee.chase@uk.ibm.com>
2024-10-15 20:58:20 +0100

    feat: global nav

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ import '@carbon/web-components/es/components/button/button.js';
 import '@carbon/web-components/es/components/ui-shell/index';
 import './style.scss';
 
+import Notification20 from '@carbon/web-components/es/icons/notification/20.js';
+import UserAvatar20 from '@carbon/web-components/es/icons/user--avatar/20.js';
+import Switcher20 from '@carbon/web-components/es/icons/switcher/20.js';
+
+export class MyIcon extends LitElement {
+  static properties = {
+    icon: { attribute: true, type: String },
+  };
+
+  render() {
+    return (
+      { Notification20, UserAvatar20, Switcher20 }[this.icon] ?? Notification20
+    )();
+  }
+}
+customElements.define('my-icon', MyIcon);
+
 export class MyCount extends LitElement {
   static properties = {
     count: { attribute: true, type: Number },
