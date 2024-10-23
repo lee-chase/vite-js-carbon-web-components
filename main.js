2f7dd68f56ecb4463b1124282ffce4c98c44729d
Lee Chase <lee.chase@uk.ibm.com>
2024-10-23 11:35:35 +0100

    feat: step 1 part 2 done

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 import './style.scss';
 import '@carbon/web-components/es/components/button/button.js';
+import '@carbon/web-components/es/components/ui-shell/index';
 
+const bodyEl = document.querySelector('body');
+
+// button click handler
 const handleClick = () => {
-  document.querySelector(':root').classList.toggle('light');
+  bodyEl.classList.toggle('g10');
+  bodyEl.classList.toggle('g100');
 };
-
 document.querySelector('.button').addEventListener('click', handleClick);
+
+// set initial theme based on preferences
+if (matchMedia('(prefers-color-scheme: dark)')) {
+  bodyEl.classList.add('g100');
+} else {
+  bodyEl.classList.add('g10');
+}
